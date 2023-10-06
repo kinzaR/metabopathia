c71da71bdec9ddc82bb8bce3c6c9af13229d7402
kinzaR <rian.kinza@gmail.com>
2023-10-06 13:14:13 +0200

    check if data has metabolites of selected pathways

--- data_examples/KEDDIDs.R
+++ data_examples/KEDDIDs.R
@@ -101,43 +101,71 @@ class(metab_df)
 
 # compare between ampollas raw formula
 library(dplyr)
-ampollas_clean_formul<- ampollas_raw
-ampollas_clean_formul$anot[!grepl(";",ampollas_clean_formul$anot)] <- sub("^(.*?)_.*$","\\1",ampollas_raw$anot[!grepl(";",ampollas_raw$anot)])
-ampollas_clean_formul$anot[grepl("; ",ampollas_clean_formul$anot)] <- sapply(strsplit(ampollas_clean_formul$anot[grepl("; ",ampollas_clean_formul$anot)]," "),
-                                                                            function(xx) {
-                                                                              x <- sub(" $", "", xx)
-                                                                              formula <-grep("_M\\+H(;)?$", x, value = TRUE)
-                                                                              if(isEmpty(formula)) {
-                                                                                formula <-grep("_M\\+NH4(;)?$", x, value = TRUE)
-                                                                                if(isEmpty(formula)){
-                                                                                  formula <-grep("_M\\+Na(;)?$", x, value = TRUE)
+get_formula_from_data<-function(ampollas_raw){
+  ampollas_clean_formul<- ampollas_raw
+  ampollas_clean_formul[!grepl(";",ampollas_clean_formul)] <- sub("^(.*?)_.*$","\\1",ampollas_raw[!grepl(";",ampollas_raw)])
+  ampollas_clean_formul[grepl("; ",ampollas_clean_formul)] <- sapply(strsplit(ampollas_clean_formul[grepl("; ",ampollas_clean_formul)]," "),
+                                                                              function(xx) {
+                                                                                x <- sub(" $", "", xx)
+                                                                                formula <-grep("_M\\+H(;)?$", x, value = TRUE)
+                                                                                if(isEmpty(formula)) {
+                                                                                  formula <-grep("_M\\+NH4(;)?$", x, value = TRUE)
+                                                                                  if(isEmpty(formula)){
+                                                                                    formula <-grep("_M\\+Na(;)?$", x, value = TRUE)
+                                                                                  }
                                                                                 }
-                                                                              }
-                                                                              if(!isEmpty(formula)) {
-                                                                                formula <- sub("^(.*?)_.*$","\\1",formula)
-                                                                                if(length(formula)==1) return(formula)
-                                                                                if(length(formula)!=1 && n_distinct(formula)){
-                                                                                  return(formula[1])
+                                                                                if(!isEmpty(formula)) {
+                                                                                  formula <- sub("^(.*?)_.*$","\\1",formula)
+                                                                                  if(length(formula)==1) return(formula)
+                                                                                  if(length(formula)!=1 && n_distinct(formula)){
+                                                                                    return(formula[1])
+                                                                                  }else{
+                                                                                    cat("\n",formula," has differents formulas options, need a manual feedback!! \n")
+                                                                                  }
                                                                                 }else{
-                                                                                  cat("\n",formula," has differents formulas options, need a manual feedback!! \n")
+                                                                                  cat("\n",x," has neither _M+H, _M+Na nor M-NH4+\nThe original formula was: \n")
+                                                                                  print(ampollas_raw[ampollas_raw==paste(xx, collapse = " "),c(1:8)])
+                                                                                  return(NA)
                                                                                 }
-                                                                              }else{
-                                                                                cat("\n",x," has neither _M+H, _M+Na nor M-NH4+\nThe original formula was: \n")
-                                                                                print(ampollas_raw[ampollas_raw$anot==paste(xx, collapse = " "),c(1:8)])
-                                                                                return(NA)
-                                                                              }
                                                                               }) %>%unlist()
+  return(ampollas_clean_formul)
+}
 
 
+ampolla_formulas<-get_formula_from_data(ampollas_raw$anot)
+#suero
+suero_data <- read.csv("data_examples/Dystrophic_epidermolysis_bullosa/Carlos_leon_hilic_positivev2 (1) (2).csv")
+suero_formulas<-get_formula_from_data(suero_data$anot)
 
+ampollas_raw$anot<-ampolla_formulas
+suero_data$anot<-suero_formulas
 
-intersect(metab_df$FORMULA, ampollas_clean_formul$anot) %>%length()
-data_for_metabo<-ampollas_clean_formul[ampollas_clean_formul$anot %in%metab_df$FORMULA, ]
+intersect(metab_df$FORMULA, ampolla_formulas) %>%length()
+intersect(metab_df$FORMULA, suero_formulas) %>%length()
 
-length(unique(data_for_metabo$anot))
-amp_mes<-metab_df[metab_df$FORMULA%in%data_for_metabo$anot,1]
-p <- metabo_pathways$pathigraphs$hsa04923
-met_per_path<-lapply(metabo_pathways$pathigraphs, function(p){
-  commun <- intersect(V(p$graph)$metaboID , amp_mes)
-  return(c(all(V(p$graph)$metaboID %in% amp_mes),paste0(length(commun)/length(V(p$graph)$metaboID), "%"),commun))
-})
+data_for_metabo_amp<-ampollas_raw[ampollas_raw$anot %in%metab_df$FORMULA, ]
+data_for_metabosuero<-suero_data[suero_data$anot %in%metab_df$FORMULA, ]
+
+length(unique(data_for_metabo_amp$anot))
+length(unique(data_for_metabosuero$anot))
+
+amp_met<-metab_df[metab_df$FORMULA%in%data_for_metabo_amp$anot,1] %>%unique()
+sue_met<-metab_df[metab_df$FORMULA%in%data_for_metabosuero$anot,1] %>%unique()
+# p <- metabo_pathways$pathigraphs$hsa04923
+library(purrr)
+met_per_path_amp<-lapply(metabo_pathways$pathigraphs, function(p){
+  commun <- intersect(V(p$graph)$metaboID , amp_met)
+  if(length(commun)>0)  return(c(all(V(p$graph)$metaboID %in% amp_met),length(commun)/length(V(p$graph)$metaboID),paste(commun,collapse = ",")))
+}) %>% purrr::compact(.)
+met_per_path_suer<-lapply(metabo_pathways$pathigraphs, function(p){
+  commun <- intersect(V(p$graph)$metaboID , sue_met)
+  if(length(commun)>0)  return(c(all(V(p$graph)$metaboID %in% sue_met),length(commun)/length(V(p$graph)$metaboID),paste(commun,collapse = ",")))
+}) %>% purrr::compact(.)
+
+inter_ampolla_data <- do.call(rbind,met_per_path_amp)%>%as.data.frame()
+inter_suero_data <- do.call(rbind,met_per_path_suer)%>%as.data.frame()
+colnames(inter_ampolla_data)<- c("fully covered", "percentage of coverage %", "detected metabolites")
+colnames(inter_suero_data)<- c("fully covered", "percentage of coverage %", "detected metabolites")
+
+write.table(x = inter_ampolla_data, file = "data_examples/Dystrophic_epidermolysis_bullosa/intersection_Kegg_ampolla.tsv", quote = F, sep = "\t", row.names = T, col.names = T)
+write.table(x = inter_suero_data, file = "data_examples/Dystrophic_epidermolysis_bullosa/intersection_Kegg_suero.tsv", quote = F, sep = "\t", row.names = T, col.names = T)
