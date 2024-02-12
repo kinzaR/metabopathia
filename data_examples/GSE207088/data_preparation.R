2db1de60ea70b4f22a6167f3f56859caf55631da
kinzaR <rian.kinza@gmail.com>
2024-02-12 17:11:09 +0100

    example GSE207088

--- data_examples/GSE207088/data_preparation.R
+++ data_examples/GSE207088/data_preparation.R
@@ -1,10 +1,11 @@
+# annotation was performed using https://www.metaboanalyst.ca/MetaboAnalyst/home.xhtml
 metabo_data <-read.table(file = "data_examples/GSE207088/polar_metab.tsv", sep = "\t", row.names = 1, header =  T)
 metab_annotation <- read.csv("data_examples/GSE207088/polar_name_metaboanalyst.csv", header=T, row.names=1, sep=",")
 metabo_data$ID<-metab_annotation[rownames(metabo_data),"KEGG"]
 metabo_data<-metabo_data[!is.na(metabo_data$ID),]
 rownames(metabo_data) <- metabo_data$ID
 metabo_data$ID <- NULL
-write.table(metabo_data, file = "data_examples/GSE207088/toBeUsed/polar_metab_normalized_annotated.tsv")
+write.table(metabo_data, file = "data_examples/GSE207088/toBeUsed/polar_metab_normalized_annotated.tsv",sep = "\t", append = F, quote = F,row.names = T,col.names = T)
 #######
 # design
 des <- data.frame(sample=colnames(metabo_data), group=c(rep("CTRL",3),rep("IPD",3)))
